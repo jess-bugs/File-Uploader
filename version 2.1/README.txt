58ace6ca2d99125f8e5ca70cf38c0cb38e192cbf
jess-bugs <84406592+jess-bugs@users.noreply.github.com>
2023-10-20 07:25:33 +0800

    Version 2.1
    Key Features:
    MySQL-based logs
    Text file-based logs
    MySQL Web GUI for logs
    Fields are required - can be bypassed when inspect was used

---
+++ version 2.1/README.txt
@@ -0,0 +1,13 @@
+
+
+Key Features:
+
+> Upload via FTP
+> text file log
+> MySQL database logs
+> real-time server logging
+
+
+New:
+> all fields are required - cannot be bypassed unless HTML-modified via inspect
+> SQL logs can be viewed via Web GUI: bleepinghost.online/FTP/logs
---
+++ version 2.1/result.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HTML-PHP Form</title>
+
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+
+
+</head>
+<body class="the-body p-2">
+
+
+
+<?php
+
+session_start();
+
+$result = $_SESSION['result'];
+
+
+?>
+
+
+<p style="font-size: 1.9em;" class="fw-bold text-center mt-5"> <?php echo $result; ?> </p>
+
+
+</body>
+</html>
